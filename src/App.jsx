db9dbe8d29217784fee69db629fef35e408f7fc6
MausVII <elanlocke@gmail.com>
2023-03-10 10:22:26 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
   
   return (
     <div className="w-full h-full flex flex-wrap justify-center">
-      <div className='bg-[url("https://images.hdqwalls.com/wallpapers/kaisa-league-of-legends-4k-artwork-v9.jpg")] h-96 w-full'>
-        <h1 className='text-white text-6xl font-bold'>This is the Headline</h1>
-        <h2 className='text-white text-3xl font-light mt-5'>Some really great stuff here.</h2>
+      <div className='bg-[url("https://images.hdqwalls.com/wallpapers/kaisa-league-of-legends-4k-artwork-v9.jpg")] h-1/2 w-full bg-cover bg-top p-16'>
+        <h1 className='text-white text-6xl font-bold'>This is League Of Legends!</h1>
+        <h2 className='text-white text-3xl font-light mt-5'>An info gallery of champions</h2>
       </div>
       {message.map((row,index) => (
       <div className="m-8 flex" > 
